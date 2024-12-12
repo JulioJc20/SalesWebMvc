9d5280a05c4ec41f5cca18707d9ea0f205e85031
Julio Cesar <juliocesarnascimento199@gmail.com>
2024-12-12 15:37:06 -0300

    Fist controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -15,6 +15,7 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult Index()
         {
+
             return View();
         }
 
@@ -22,6 +23,7 @@ namespace SalesWebMvc.Controllers
         {
             return View();
         }
+      
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
