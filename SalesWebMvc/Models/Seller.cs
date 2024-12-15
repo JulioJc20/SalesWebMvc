cad0cb7666a30e7bead4fdb94192641b5a8ab2e8
Julio Cesar <juliocesarnascimento199@gmail.com>
2024-12-15 15:56:41 -0300

    Other entities and second migration

---
+++ SalesWebMvc/Models/Seller.cs
@@ -0,0 +1,49 @@
+﻿using System.Xml.Serialization;
+
+namespace SalesWebMvc.Models
+{
+    public class Seller
+    {
+        public int Id  { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate  { get; set; }
+        public double BaseSalary{ get; set; }
+
+        public Department Department { get; set; }
+
+        public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            BirthDate = birthDate;
+            BaseSalary = baseSalary;
+            Department = department;
+        }
+
+        public Seller()
+        {
+        }
+
+        public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+
+        public void AddSales(SalesRecord sr)
+        {
+            Sales.Add(sr);
+        }
+
+        public void RemoveSales(SalesRecord sr)
+        {
+            Sales.Remove(sr);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sales.Where(sr => sr.Date >= initial && sr.Date <= final).Sum(sr => sr.Amount);
+        }
+
+
+
+    }
+}
