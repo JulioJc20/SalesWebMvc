ba3eb88e7795d3a86625cd1aaf9419bc2999fcf8
Julio Cesar <juliocesarnascimento199@gmail.com>
2024-12-17 19:31:07 -0300

    SellerService and basic FindAll

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -1,19 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Services;
 
 namespace SalesWebMvc.Controllers
 {
     public class SellersController : Controller
     {
+
+        private readonly SellerService _sellerService;
+
+        public SellersController(SellerService selerSercive)
+        {
+            _sellerService = selerSercive; 
+        }
         public IActionResult Index()
         {
-            return View();
+            var list = _sellerService.FindAll();
+            return View(list);
         }
 
-
-
-
-
-
+  
 
 
 
--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using SalesWebMvc.Data;
-
+using SalesWebMvc.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,6 +15,7 @@ builder.Services.AddDbContext<SalesWebMvcContext>(options =>
 );
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddScoped <SellerService>();
 
 var app = builder.Build();
 
